fc1c122560a49fbc4d58d0fa3a817e57c2414235
Vasileios Klouvatos <bilklo@gmail.com>
2015-03-22 09:45:57 +0100

    Add plot1 R implementation

---
+++ plot1.R
@@ -0,0 +1,18 @@
+plot1 <- function()
+{
+  #load the NEI
+  NEI <- readRDS("summarySCC_PM25.rds")
+  
+  #load the SCC
+  SCC <- readRDS("Source_Classification_Code.rds")
+  
+  #summary all the emissions per year from all sources
+  summOfEmissions <- ddply(NEI,.(year), summarise,sum(Emissions))
+  
+  #Create the plot in png file
+  png("plot1.png",width = 480, height = 480, bg="transparent")
+  plot(summOfEmissions$year,summOfEmissions$..1, type='l', 
+       ylab = "Total Emissions", xlab = "Years")
+  #close
+  dev.off()
+}
