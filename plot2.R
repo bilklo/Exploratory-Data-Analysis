e22245be787e5ec246be072120ace0ab13e639a1
Vasileios Klouvatos <bilklo@gmail.com>
2015-03-22 11:33:23 +0100

    fix error

--- plot2.R
+++ plot2.R
@@ -6,11 +6,14 @@ plot2 <- function()
   #load the SCC
   SCC <- readRDS("Source_Classification_Code.rds")
   
+  #Filter the data based in fips=24510
+  select<-filter(NEI,fips=="24510")
+  
   #summary all the emissions per year from all sources
-  summOfEmissions <- ddply(NEI,.(year), summarise,sum(Emissions))
+  summOfEmissions <- ddply(select,.(year), summarise,sum(Emissions))
   
   #Create the plot in png file
-  png("plot1.png",width = 480, height = 480, bg="transparent")
+  png("plot2.png",width = 480, height = 480, bg="transparent")
   plot(summOfEmissions$year,summOfEmissions$..1, type='l', 
        ylab = "Total Emissions", xlab = "Years")
   #close
